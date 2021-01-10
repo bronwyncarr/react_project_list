4ec3a97577ab15dae12078f542cbcbce51e4de12
bronwyncarr <bronwynacarr@gmail.com>
2021-01-10 17:04:20 +1100

    adds project data array

---
+++ src/ProjectData.js
@@ -0,0 +1,17 @@
+const projects = [
+  {
+    name: "Sets",
+    description: "A React version of the popular logic game",
+  },
+  {
+    name: "Chore Tracker",
+    description: "A full stack app to track chores completed and money earned",
+  },
+  {
+    name: "Fotozu",
+    description:
+      "A full stack app using AWS lambda for serverless backend that showcases the photography of a client",
+  },
+];
+
+export default projects;
